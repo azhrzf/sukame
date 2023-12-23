ec9422d94ac752e7acc8d044e35da7a983420221
mfarhan08a <farhanputra.farhan8@gmail.com>
2023-12-23 21:24:41 +0700

    add getchatlist, sendchat, and getchatws function

--- src/components/Context.tsx
+++ src/components/Context.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useContext, useState } from "react";
 import { ChatProps } from "../types";
 import { dummychats } from "../data";
+// import axios from "axios";
 
 const useChats = (initial: ChatProps[] = []) => {
 	const [chats, setChats] = useState(initial);
@@ -31,3 +32,68 @@ export function useChatContext() {
 	}
 	return context;
 }
+
+// let baseURL = "";
+
+// const sendChat = (conversation_id: string, message: string) => {
+// 	axios
+// 		.post(
+// 			`{baseURL}/v1/chat/${conversation_id}`,
+// 			{
+// 				message: message,
+// 				datetime: new Date().toISOString(),
+// 				user: {
+// 					id: 123,
+// 					name: "Hachiman",
+// 				},
+// 			},
+// 			{
+// 				headers: { Authorization: "Bearer " + Cookies.get("token") },
+// 			}
+// 		)
+// 		.then((res) => {
+// 			let { data } = res;
+// 			console.log(data);
+// 			let newChat = data;
+
+// 			setChats((prevChats) => {
+// 				const updatedChats = [...prevChats, newChat];
+// 				return updatedChats;
+// 			});
+// 		})
+// 		.catch((error) => {
+// 			console.log(error.response.data.error);
+// 			alert(error.response.data.error);
+// 		});
+// };
+
+// const getChatList = async (id: string) => {
+// 	let result = await axios.get(`{base_url}/v1/chat/:${id}?q=dia&limit=20&page=1`, {
+// 		headers: { Authorization: "Bearer " + Cookies.get("token") },
+// 	});
+// 	let fetchResult = result.data.data;
+
+// 	setChats(fetchResult);
+// };
+
+// export function getChatData() {
+// 	const ws = new WebSocket(`ws://${baseURL}/v1/conversations/ws`);
+
+// 	ws.onopen = () => {
+// 		console.log("Websocket connection opened");
+// 	};
+
+// 	ws.onmessage = (event) => {
+// 		const newChat = JSON.parse(event.data);
+// 		setChats((prevChats) => {
+// 			const updatedChats = [...prevChats, newChat];
+// 			return updatedChats;
+// 		});
+// 	};
+
+// 	ws.onerror = (error) => {
+// 		console.log(error);
+// 	};
+
+// 	return ws;
+// }
