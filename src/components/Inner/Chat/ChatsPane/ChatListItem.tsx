87d2d4893bebda85fce5d1b22e9bbdaaa0d9e134
Azhar Zaidan Fauzi <a.zaidan.f.azf616@gmail.com>
2024-01-02 10:41:28 +0700

    clean structure

---
+++ src/components/Inner/Chat/ChatsPane/ChatListItem.tsx
@@ -0,0 +1,99 @@
+import * as React from "react";
+import Box from "@mui/joy/Box";
+import ListDivider from "@mui/joy/ListDivider";
+import ListItem from "@mui/joy/ListItem";
+import ListItemButton, { ListItemButtonProps } from "@mui/joy/ListItemButton";
+import Stack from "@mui/joy/Stack";
+import Typography from "@mui/joy/Typography";
+// import CircleIcon from "@mui/icons-material/Circle";
+import AvatarWithStatus from "../MessagesPane/AvatarWithStatus";
+import { ChatProps, MessageProps, UserProps } from "../../../../types";
+import { toggleMessagesPane } from "../../../../utils";
+import { getSender } from "../../../../data";
+// import { Chat, Message, User } from "./context";
+
+type ChatListItemProps = ListItemButtonProps & {
+  id: string;
+  unread?: boolean;
+  sender: UserProps[];
+  messages: MessageProps[];
+  user: UserProps;
+  selectedChatId?: string;
+  setSelectedChat: (chat: ChatProps) => void;
+};
+
+function ChatListItem({
+  id,
+  sender,
+  messages,
+  user,
+  selectedChatId,
+  setSelectedChat,
+}: ChatListItemProps) {
+  const selected = selectedChatId === id;
+
+  return (
+    <React.Fragment>
+      <ListItem>
+        <ListItemButton
+          onClick={() => {
+            toggleMessagesPane();
+            setSelectedChat({ id, sender, messages });
+          }}
+          selected={selected}
+          color="neutral"
+          sx={{
+            flexDirection: "column",
+            alignItems: "initial",
+            gap: 1,
+          }}
+        >
+          <Stack direction="row" spacing={1.5}>
+            <AvatarWithStatus
+              online={getSender(user, sender).online}
+              src={getSender(user, sender).avatar}
+            />
+            <Box sx={{ flex: 1 }}>
+              <Typography level="title-sm">
+                {getSender(user, sender).name}
+              </Typography>
+              <Typography level="body-sm">
+                {getSender(user, sender).username}
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                lineHeight: 1.5,
+                textAlign: "right",
+              }}
+            >
+              {/* {messages[0].unread && <CircleIcon sx={{ fontSize: 12 }} color="primary" />} */}
+              <Typography
+                level="body-xs"
+                display={{ xs: "none", md: "block" }}
+                noWrap
+              >
+                5 mins ago
+              </Typography>
+            </Box>
+          </Stack>
+          <Typography
+            level="body-sm"
+            sx={{
+              display: "-webkit-box",
+              WebkitLineClamp: "2",
+              WebkitBoxOrient: "vertical",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            }}
+          >
+            {messages.length > 0 && messages[messages.length - 1].content}
+          </Typography>
+        </ListItemButton>
+      </ListItem>
+      <ListDivider sx={{ margin: 0 }} />
+    </React.Fragment>
+  );
+}
+
+export default ChatListItem;
---
+++ src/components/Inner/Chat/MessagesPane/index.tsx
@@ -0,0 +1,107 @@
+import * as React from "react";
+import Box from "@mui/joy/Box";
+import Sheet from "@mui/joy/Sheet";
+import Stack from "@mui/joy/Stack";
+import AvatarWithStatus from "./AvatarWithStatus";
+import ChatBubble from "./ChatBubble";
+import MessageInput from "./MessageInput";
+import MessagesPaneHeader from "./MessagesPaneHeader";
+import { ChatProps, MessageProps, UserProps } from "../../../../types";
+import { getSender } from "../../../../data";
+
+type MessagesPaneProps = {
+  user: UserProps;
+  selectedChat: ChatProps;
+  handleChatSend: (currentMessage: MessageProps) => void;
+};
+
+function MessagesPane({
+  user,
+  selectedChat,
+  handleChatSend,
+}: MessagesPaneProps) {
+  const [currentChatMessages, setCurrentChatMessages] = React.useState(
+    selectedChat.messages
+  );
+  const [textAreaValue, setTextAreaValue] = React.useState("");
+
+  React.useEffect(() => {
+    setCurrentChatMessages(selectedChat.messages);
+  }, [selectedChat.messages]);
+
+  const onEnterHandler = () => {
+    if (!textAreaValue) {
+      return;
+    }
+    const newId = currentChatMessages.length + 1;
+    const newIdString = newId.toString();
+    const newMessage: MessageProps = {
+      id: newIdString,
+      sender: user,
+      content: textAreaValue,
+      timestamp: "Just now",
+    };
+    setTextAreaValue("");
+    setCurrentChatMessages([...currentChatMessages, newMessage]);
+    handleChatSend(newMessage);
+  };
+
+  return (
+    <Sheet
+      sx={{
+        height: { xs: "calc(100dvh - var(--Header-height))", lg: "100dvh" },
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "background.level1",
+      }}
+    >
+      <MessagesPaneHeader sender={getSender(user, selectedChat.sender)} />
+
+      <Box
+        sx={{
+          display: "flex",
+          flex: 1,
+          minHeight: 0,
+          px: 2,
+          py: 3,
+          overflowY: "scroll",
+          flexDirection: "column-reverse",
+        }}
+      >
+        <Stack spacing={2} justifyContent="flex-end">
+          {currentChatMessages.map((message: MessageProps, index: number) => {
+            const isYou = message.sender.id === user.id;
+            return (
+              <Stack
+                key={index}
+                direction="row"
+                spacing={2}
+                flexDirection={isYou ? "row-reverse" : "row"}
+              >
+                {message.sender.id !== user.id && (
+                  <AvatarWithStatus
+                    online={message.sender.online}
+                    src={message.sender.avatar}
+                  />
+                )}
+                <ChatBubble
+                  variant={isYou ? "sent" : "received"}
+                  {...message}
+                  user={user}
+                />
+              </Stack>
+            );
+          })}
+        </Stack>
+      </Box>
+
+      <MessageInput
+        textAreaValue={textAreaValue}
+        setTextAreaValue={setTextAreaValue}
+        onSubmit={onEnterHandler}
+      />
+    </Sheet>
+  );
+}
+
+export default MessagesPane;
